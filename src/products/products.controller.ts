880d1859ef8d6c3af6fd7990ce9ef5af6156d795
Dev Shah <busycaesar@gmail.com>
2024-03-21 19:13:50 -0400

    Feat(Products): RESTApi for products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,16 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { Product } from './products.model';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  getAllProducts(): Product[] {
+    return this.productsService.getAllProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') id: number): any {
+    if (!id) return { Error: 'Invalid Id provided' };
+    return this.productsService.getProduct(id);
+  }
 
   @Post()
   createProduct(
     @Body('title') prodTitle: string,
     @Body('description') prodDescription: string,
     @Body('price') prodPrice: number,
-  ): { id: number } {
+  ): any {
+    // Make sure all the required data is received!
+    if (!prodTitle || !prodDescription || !prodPrice)
+      return { Error: 'Data not found!' };
     const productId = this.productsService.createProduct(
       prodTitle,
       prodDescription,
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,7 +3,17 @@ import { Product } from './products.model';
 
 @Injectable()
 export class ProductsService {
-  products: Product[] = [];
+  private products: Product[] = [];
+
+  getAllProducts() {
+    return [...this.products];
+  }
+
+  getProduct(id: number) {
+    const product = this.products.find((product) => product.id == id);
+    return product;
+  }
+
   createProduct(title: string, description: string, price: number): number {
     const newProductId: number = new Date().getTime();
     const newProduct = new Product(newProductId, title, description, price);
